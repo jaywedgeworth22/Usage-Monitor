b1ee6fdc8f734bee944861548da63c28315912e2
Jay Wedgeworth <12656028+jaywedgeworth22@users.noreply.github.com>
2026-07-11 21:11:07 -0500

    feat: Safari Web Extension wrapper for browser sync

---
+++ safari-extension/API Usage Monitor/API Usage Monitor/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  API Usage Monitor
+//
+//  Created by Jay Wedgeworth on 7/11/26.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        // Override point for customization after application launch.
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+}
